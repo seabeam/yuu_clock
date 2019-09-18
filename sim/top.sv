38bd0a5da427810af070e3137ec510842275a1e5
wind <seabeam@qq.com>
2019-09-18 21:35:24 +0800

    add another example interface for drive update

--- sim/top.sv
+++ sim/top.sv
@@ -9,10 +9,12 @@ import yuu_common_pkg::*;
 import yuu_clock_pkg::*;
 
 class uvc_test extends uvm_test;
-  virtual yuu_clock_interface vif;
+  virtual yuu_clock_interface clk_vif0;
+  virtual yuu_clock_interface clk_vif1;
   uvm_event_pool events;
 
-  yuu_clock_agent agent;
+  yuu_clock_agent agent0;
+  yuu_clock_agent agent1;
 
   `uvm_component_utils(uvc_test)
 
@@ -21,24 +23,36 @@ class uvc_test extends uvm_test;
   endfunction
 
   function void build_phase(uvm_phase phase);
-    yuu_clock_config cfg = new("cfg");
+    yuu_clock_config cfg0 = new("cfg0");
+    yuu_clock_config cfg1 = new("cfg1");
 
     events = new("events");
-    uvm_config_db#(virtual yuu_clock_interface)::get(null, get_full_name(), "vif", cfg.vif);
-    cfg.set_freq(72, 32);
-    cfg.set_duty(0.3);
-    cfg.enable_clock_slow(True);
-    cfg.enable_clock_gating(True);
-    cfg.events = events;
-    uvm_config_db#(yuu_clock_config)::set(this, "agent", "cfg", cfg);
-    agent = new("agent", this);
+    uvm_config_db#(virtual yuu_clock_interface)::get(null, get_full_name(), "vif0", cfg0.vif);
+    cfg0.set_freq(2, 1);
+    cfg0.set_duty(0.3);
+    cfg1.set_unit("M");
+    cfg0.enable_clock_slow(True);
+    cfg0.enable_clock_gating(True);
+    cfg0.events = events;
+    uvm_config_db#(yuu_clock_config)::set(this, "agent0", "cfg", cfg0);
+    agent0 = new("agent0", this);
+
+    uvm_config_db#(virtual yuu_clock_interface)::get(null, get_full_name(), "vif1", cfg1.vif);
+    cfg1.set_freq(800);
+    cfg1.set_duty(0.5);
+    cfg1.set_unit("K");
+    cfg1.enable_clock_slow(False);
+    cfg1.enable_clock_gating(False);
+    cfg1.events = events;
+    uvm_config_db#(yuu_clock_config)::set(this, "agent1", "cfg", cfg1);
+    agent1 = new("agent1", this);
   endfunction
 
   task main_phase(uvm_phase phase);
     fork
       begin
         phase.raise_objection(this);
-        #10000;
+        #100000;
         phase.drop_objection(this);
       end
       event_process();
@@ -63,23 +77,25 @@ class uvc_test extends uvm_test;
 endclass
 
 module top;
-  yuu_clock_interface cif();
+  yuu_clock_interface cif0();
+  yuu_clock_interface cif1();
 
   initial begin
-    uvm_config_db#(virtual yuu_clock_interface)::set(null, "", "vif", cif);
+    uvm_config_db#(virtual yuu_clock_interface)::set(null, "", "vif0", cif0);
+    uvm_config_db#(virtual yuu_clock_interface)::set(null, "", "vif1", cif1);
     run_test("uvc_test");
   end
 
   initial begin
-    cif.enable = 1'b0;
-    cif.slow   = 1'b1;
+    cif0.enable = 1'b0;
+    cif0.slow   = 1'b1;
     #55;
-    cif.enable = 1'b1;
+    cif0.enable = 1'b1;
     while(1) begin
-      #60;
-      cif.slow   = $urandom();
-      #50;
-      cif.enable = $urandom();
+      #6000;
+      cif0.slow   = $urandom();
+      #5000;
+      cif0.enable = $urandom();
     end
   end
 endmodule
